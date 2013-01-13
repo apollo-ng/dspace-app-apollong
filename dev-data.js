d33d5136d43e8fc7b2ba0fa53b2622cfc0f8f279
elf Pavlik <perpetual-tripper@wwelves.org>
2013-01-13 12:56:45 +0100

    dev data cleanup

--- dev-data.js
+++ dev-data.js
@@ -1,62 +1,48 @@
 window.data = {
   "type": "FeatureCollection",
   "name": "Hackerspaces",
-  "icon":"black-shield",
   "features": [
-
-        {
-          "id":"1",
-          "type":"Feature",
-          "icon":"black-shield",
-          "geometry": {
-				"type":"Point",
-				"coordinates":[11.56082, 48.153642]
-		  },
-		  "properties":{
-				"type":"Hackerspace",
-				"date_time":"2013-01-05 21:45:00",
-				"title":"MuCCC",
-				"description":"Chaos Computer Club München",
-				"address":"Schleissheimer Str. 90",
-				"zip_code":"80123"
-			}
-		},
-
-        {
-          "id":"2",
-          "type":"Feature",
-          "icon":"black-shield",
-          "geometry": {
-				"type":"Point",
-				"coordinates":[11.54209, 48.148964]
-		  },
-		  "properties":{
-				"type":"Hackerspace",
-				"date_time":"2013-01-05 21:46:00",
-				"title":"FabLab",
-				"description":"Another Hackerspace",
-				"address":"Elvirastr. 19",
-				"zip_code":"80124"
-			}
-		},
-
-        {
-          "id":"3",
-          "type":"Feature",
-          "icon":"black-shield",
-          "geometry": {
-				"type":"Point",
-				"coordinates":[11.70218, 48.112193]
-		  },
-		  "properties":{
-				"type":"Hackerspace",
-				"date_time":"2013-01-05 21:47:00",
-				"title":"Apollo-NG",
-				"description":"This Hackerspace",
-				"address":"Swakopmunderstr. 1",
-				"zip_code":"81827"
-			}
-		},
-
-      ]
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.56082, 48.153642]
+      },
+      "properties": {
+        "type": "Hackerspace",
+        "title": "MuCCC",
+        "description": "Chaos Computer Club München",
+        "address": "Schleissheimer Str. 90",
+        "zip_code": "80123"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.54209, 48.148964]
+      },
+      "properties": {
+        "type": "Hackerspace",
+        "title": "FabLab",
+        "description": "Another Hackerspace",
+        "address": "Elvirastr. 19",
+        "zip_code": "80124"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.70218, 48.112193]
+      },
+      "properties": {
+        "type": "Hackerspace",
+        "title": "Apollo-NG",
+        "description": "This Hackerspace",
+        "address": "Swakopmunderstr. 1",
+        "zip_code": "81827"
+      }
+    }
+  ]
 };

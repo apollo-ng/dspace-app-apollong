42cc39d4940e1f8c623d02a53b335935e650ee37
elf Pavlik <perpetual-tripper@wwelves.org>
2013-01-16 17:57:44 +0100

    moved config options to main.js

--- app/js/main.js
+++ app/js/main.js
@@ -9,12 +9,33 @@ Handlebars.registerHelper('shortPos', function(object) {
 });
 
 /**
+ * config for initializing DSpace Wrorld
+ */
+var config = {
+  geoFeeds: [
+    { name: 'Hackerspaces Munich', url: 'http://localhost:3333/hackerspaces-munich.json'},
+    { name: 'OpenWiFi Munich', url: 'http://localhost:3333/openwifi-munich.json'}
+  ],
+
+  map: {
+    tileSet: {
+        template: 'http://dspace.ruebezahl.cc:8888/v2/DSpace-tactical/{Z}/{X}/{Y}.png'
+    },
+    geolat:  48.115293,
+    geolon:  11.60218,
+    minZoom: 13,
+    maxZoom: 17,
+    defaultZoom: 12
+  }
+};
+
+/**
  * BIG BANG!
  */
 $.domReady(function (){
 
   var world = new DSpace();
-  world.init();
+  world.init(config);
 
 });
 

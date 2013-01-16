1d5dda4767b2464325e5751fc6f12427bf5d0ba8
elf Pavlik <perpetual-tripper@wwelves.org>
2013-01-16 16:53:22 +0100

    updated comnents

--- app/js/dspace.js
+++ app/js/dspace.js
@@ -1,22 +1,32 @@
+/**
+ * TODO document
+ */
 var DSpace = function(){
 
-  this.init = function () {
+  /**
+   * TODO document
+   */
+  this.init = function (){
 
-    //get packages from ender
-    //FIXME document ex deoes order matter?
+    /**
+     * require dependencies with Ender
+     * FIXME document (ex deoes order matter)?
+     */
     var Backbone = require('backbone');
     var _ = require('underscore');
     var Reqwest = require('reqwest');
-    /*
+
+    /**
      * single geographical featue of interest
      * with option to set from geoJSON feature object
      */
     var Feature = Backbone.Model.extend({
-      initialize: function() {
+
+      initialize: function(){
         this.setLatLon();
       },
 
-      /*
+      /**
        * helper method for setting lat: lon: attributes from coordinates array
        */
       setLatLon: function(){
@@ -28,21 +38,23 @@ var DSpace = function(){
     });
 
 
-    /*
-     * Add basic user model
+    /**
+     * Add basic user model FIXME
      */
     var User = Backbone.Model.extend({
-      initialize: function() {
+
+      initialize: function(){
         this.world = this.get('world');
       }
     });
 
-    /*
+    /**
      * main UI logic for the Map
      */
     var Map = Backbone.View.extend({
 
       initialize: function(){
+
           /**
            * to use with map.world FIXME
            */
@@ -64,18 +76,26 @@ var DSpace = function(){
        */
       render: function(){
 
-        // crate frame -- uses MapBox
+        /**
+         * crate frame -- uses MapBox
+         */
         this.frame = this.createFrame();
 
-        // create FeatureBox
+        /**
+         *  create FeatureBox
+         */
         this.featureBox = new FeatureBox({ map: this });
 
-        // create StatusPanel
-        // set statusPanel model to user
+        /**
+         * create StatusPanel
+         * set statusPanel model to user
+         */
         this.statusPanel = new StatusPanel({model: this.world.user, map: this });
         this.statusPanel.render();
 
-        // set overlays
+        /**
+         * set overlays
+         */
         var self = this;
 
         _(this.world.collections).each(function(featureCollection){
@@ -97,15 +117,22 @@ var DSpace = function(){
 
         var modestmap = new modestmaps.Map('map', layer);
 
-        // setup boundaries
+        /**
+         *  setup boundaries
+         */
         modestmap.setZoomRange(config.minZoom, config.maxZoom);
         var location = new modestmaps.Location(config.geolat, config.geolon);
-        // show and zoom map
+
+        /**
+         * show and zoom map
+         */
         modestmap.setCenterZoom(location, config.defaultZoom);
 
-        // FIXME add modestmap.addCallback('drawn', function(m){});
-
+        /**
+         * FIXME add modestmap.addCallback('drawn', function(m){});
+         */
         return modestmap;
+
       },
 
       /**
@@ -113,13 +140,18 @@ var DSpace = function(){
        */
       jumpToFeature: function( feature ) {
 
-        // easey interaction library for modestmaps
+        /**
+         * easey interaction library for modestmaps
+         */
         var self = this;
         var mmCoordinate = this.frame.locationCoordinate({
           lat: feature.get('lat'),
           lon: feature.get('lon')
         });
 
+        /**
+         * TODO document
+         */
         easey().map(self.frame)
         .to(mmCoordinate)
         .zoom(self.config.maxZoom).optimal();
@@ -131,13 +163,13 @@ var DSpace = function(){
       getCenter: function( ){
         return this.frame.getCenter();
       }
-
     });
 
-    /*
+    /**
      * UI element with information about feature
      */
     var FeatureBoxItem = Backbone.View.extend({
+
       className: 'featureBoxItem',
 
       initialize: function(){
@@ -149,14 +181,19 @@ var DSpace = function(){
 
       render: function(){
 
-        // get template data from model
+        /**
+         * get template data from model
+         */
         var templateData = this.model.toJSON();
 
-        // add markerLetter passed from options
+        /**
+         * add markerLetter passed from options
+         */
         templateData.markerLetter = this.options.markerLetter;
 
         $(this.el).html(this.template(templateData));
         return this.el;
+
       },
 
       events: {
@@ -172,10 +209,11 @@ var DSpace = function(){
     });
 
 
-    /*
+    /**
      * UI element with list of features
      */
     var FeatureBox = Backbone.View.extend({
+
       el: $('#featureBox'),
 
       initialize: function(){
@@ -183,36 +221,43 @@ var DSpace = function(){
       },
 
       render: function( collection ){
-        var that = this;
-
-        // DEC value of ascii "a" to "z" for marker lettering
+        var self = this;
         var letter = 97;
-        var lastletter = 122;
+        var lastletter = 122;  //FIXME DEC value of ascii "a" to "z" for marker lettering
 
 
-        /* Loop through each feature in the model
+        /**
+         * Loop through each feature in the model
          * example how to add more data to the view:
          *
          * The additionally passend markerLetter ends up in
          * the featureBoxItem as Options.markerLetter.
          */
         _(collection.models).each(function(feature, i){
-
           var markerLetter = String.fromCharCode(letter+i);
-          var featureBoxItem= new FeatureBoxItem({model: collection.models[i], map: that.map, markerLetter: markerLetter });
+          var featureBoxItem= new FeatureBoxItem({
+              model: collection.models[i]
+            , map: self.map
+            , markerLetter: markerLetter
+          });
           var renderedTemplate = featureBoxItem.render();
 
-          // here it gets added to DOM
-          $(that.el).append(renderedTemplate);
+          /**
+           * here it gets added to DOM
+           */
+          $(self.el).append(renderedTemplate);
+
         });
       }
     });
 
-    /* binds to FeatureCollection reset events.
+    /**
+     * binds to FeatureCollection reset events.
      * adds the collection to the listbox
      * draws marker with mapbox
      */
     var Overlay = Backbone.View.extend({
+
       initialize: function(){
           this.map = this.options.map;
           var self = this;
@@ -221,12 +266,19 @@ var DSpace = function(){
             self.map.featureBox.render( self.collection );
           });
       },
+
       render: function(){
-        //
-        // Add markers
-        // mapbox lib NOT same as ModestMap
+
+        /**
+         * Add markers
+         * mapbox lib NOT same as ModestMap
+         */
         var markerLayer = mapbox.markers.layer();
 
+        /**
+         * define a foctory to make markers
+         * FIXME use backbone views?
+         */
         markerLayer.factory(function(feature){
           var img = document.createElement('img');
           img.className = 'marker-image';
@@ -234,31 +286,39 @@ var DSpace = function(){
           return img;
         });
 
-        // display markers
-        // .extent() called to redraw map!
+        /**
+         * display markers
+         * .extent() called to redraw map!
+         */
         markerLayer.features(this.collection.toJSON());
         this.map.frame.addLayer(markerLayer).setExtent(markerLayer.extent());
+
       },
     });
 
-     /*
+    /**
      * UI element to show current position in botttom left
      */
     var StatusPanel = Backbone.View.extend({
+
       el: $('#statusPanel'),
 
       initialize: function(){
         _.bindAll(this, 'render');
 
-        // create convienience accessor
+        /**
+         * create convienience accessors
+         */
         this.user = this.model;
         this.map = this.options.map;
 
         this.template = Handlebars.compile($('#statusPanel-template').html());
       },
 
-      //TODO listen to changes on model (User)
-      //TODO listen on map changing it's center
+      /**
+       * TODO listen to changes on model (User)
+       * TODO listen on map changing it's center
+       */
       render: function(){
         var mapCenter = this.map.getCenter();
         var mapData = { lat: mapCenter.lat, lon: mapCenter.lon };
@@ -266,27 +326,31 @@ var DSpace = function(){
         $(this.el).html(this.template(templateData));
         return this.el;
       }
-
     });
+
     var FeatureCollection = Backbone.Collection.extend({
-        model: Feature,
-        /* requests the geojson
-         * resets ifselft with the result
-         */
-        sync: function(){
-          var self = this;
-          new Reqwest({
-            url: this.url,
-            success: function( response ) {
-              self.reset( response.features ); },
+
+      model: Feature,
+
+      /**
+       * requests the geojson
+       * resets ifselft with the result
+       */
+      sync: function(){
+        var self = this;
+        var request = new Reqwest({
+          url: this.url,
+          success: function( response ) {
+            self.reset( response.features ); },
             failure: function( e ) {
               alert( '#FIXME' ); }
-          });
-        }
-      });
+        });
+      }
+    });
 
     var World = Backbone.Model.extend({
-      /*
+
+      /**
        * Genesis ;)
        */
       initialize: function(){
@@ -302,20 +366,22 @@ var DSpace = function(){
          */
         this.collections = [];
 
-        // FIXME proper way for setting initial set of overlays
+        /**
+         * FIXME proper way for setting initial set of overlays
+         */
         _(this.geoFeeds).each(function(geoFeed){
           self.addFeatureCollection(geoFeed);
         });
 
         /**
-         * create Map
+         * create and render Map
          */
         this.map = new Map({world: this, config: this.mapConfig});
         this.map.render();
       },
 
-      /** expects GeoFeed and returns FeatureCollection
-       *
+      /**
+       * expects GeoFeed and returns FeatureCollection
        */
       addFeatureCollection: function( geoFeed ){
         var featureCollection = new FeatureCollection( );
@@ -327,14 +393,15 @@ var DSpace = function(){
         return featureCollection;
       },
 
-
+      /**
+       * FIXME pass as argument from domOnReady
+       */
       geoFeeds: [
         { name: 'Hackerspaces Munich', url: 'http://localhost:3333/hackerspaces-munich.json'},
         { name: 'OpenWiFi Munich', url: 'http://localhost:3333/openwifi-munich.json'}
       ],
 
       mapConfig: {
-
         tileSet: {
             template: 'http://dspace.ruebezahl.cc:8888/v2/DSpace-tactical/{Z}/{X}/{Y}.png'
         },
@@ -346,28 +413,28 @@ var DSpace = function(){
       }
     });
 
-
-    /*
-     * BIG BANG!
+    /**
+     * init() returns an instance of a World
      */
     return new World();
 
-    /**
-     * for ease of debugging
-     *
-     * !!! NEVER COMMIT UNCOMMENTED !!!
-     */
-    //window.world = world;
-
   };
 
+  /**
+   * returns itself
+   */
   return this;
 
 };
 
-$.domReady(function () {
+/**
+ * BIG BANG!
+ */
+$.domReady(function (){
+
   var world = new DSpace();
   world.init();
+
 });
 
 

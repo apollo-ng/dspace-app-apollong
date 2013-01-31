761d4f53a548ae742dbe63e3eea291daaa4a9b08
elf Pavlik <perpetual-tripper@wwelves.org>
2013-01-31 09:27:29 +0100

    annotated config

--- app/helpers/ms2kmh.js
+++ app/helpers/ms2kmh.js
@@ -1,8 +1,10 @@
-/**
- * Geo API delivers m/s -> convert to km/h for drive modes
- */
 define(['handlebars'], function(Handlebars) {
 
+/**
+ * Method: ms2kmh
+ *
+ * Geo API delivers m/s -> convert to km/h for drive modes
+ */
   function ms2kmh(speed) {
     if ( typeof speed  !== 'undefined') {
       return ((speed * 3600)/1000 + 'km/h');

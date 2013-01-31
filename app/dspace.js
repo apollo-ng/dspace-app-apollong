ba7b67186741f54f644d71e0a0ce7164e73ecf21
elf Pavlik <perpetual-tripper@wwelves.org>
2013-01-31 04:42:45 +0100

    added Dspace file

--- app/dspace.js
+++ app/dspace.js
@@ -2,6 +2,11 @@ define([
   'models/world'
 ], function(World) {
 
+  /**
+   * Class: DSpace
+   *
+   * it doesn't matter ;)
+   */
   return {
     init: function(config) {
       return new World({ config : config });

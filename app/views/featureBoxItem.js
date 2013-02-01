73f1294f8a3966a1ac8df2090f7852266719ec00
() <nilclass@riseup.net>
2013-02-01 23:19:04 +0100

    moved 'updateSettings' (and data-format=position logic) from FeatureBoxItem to UI

--- app/views/featureBoxItem.js
+++ app/views/featureBoxItem.js
@@ -1,4 +1,4 @@
-define(['backbone', 'templateMap', 'template/helpers/renderPos'], function(Backbone, templates, renderPos) {
+define(['backbone', 'templateMap'], function(Backbone, templates) {
 
   /**
    * Class: FeatureBoxItem
@@ -45,17 +45,6 @@ define(['backbone', 'templateMap', 'template/helpers/renderPos'], function(Backb
     },
 
     initialize: function(opts) {
-      opts.aether.on('user:change', this.updateSettings.bind(this));
-      setTimeout(function() {
-        this.updateSettings(opts.aether.user);
-      }.bind(this), 0);
-    },
-
-    updateSettings: function(user) {
-      this.$('*[data-format=position]').forEach(function(e) {
-        var el = this.$(e);
-        el.text(renderPos(el.attr('data-lat'), el.attr('data-lon'), user.get('userCoordPrefs')));
-      }.bind(this));
     },
 
     /**

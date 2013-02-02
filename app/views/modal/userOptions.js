2f8eba48932e57487bbfb8c5575a881b580abe90
() <nilclass@riseup.net>
2013-02-02 16:50:28 +0100

    removed fragment-based accordion functionality from userOptionModal

--- app/views/modal/userOptions.js
+++ app/views/modal/userOptions.js
@@ -1,8 +1,9 @@
 define([
+  'ender',
   'backbone',
   'views/modal/base',
   'templateMap',
-], function(Backbone, BaseModal, templates) {
+], function($, Backbone, BaseModal, templates) {
 
     /**
      * Class: Modal.UserOptions
@@ -16,7 +17,8 @@ define([
       template: templates.userOptionModal,
 
       events: {
-        'change input': 'fireChanges'
+        'change input': 'fireChanges',
+        'click section > h2 > a': 'setCurrentSection'
       },
 
       initialize: function(options) {
@@ -40,12 +42,27 @@ define([
             }.bind(this));
           }
         }.bind(this), 0);
+
+        $(window).on('resize', this.updateSectionHeight.bind(this));
       },
 
       fireChanges: function(event) {
         var target = this.$(event.target);
         this.user.set(target.attr('name'), target.val());
         this.user.save();
+      },
+
+      setCurrentSection: function(event) {
+        var section = $(event.target).closest('section');
+        this.$('section.active').removeClass('active').attr('style', '');
+        section.addClass('active');
+        this.updateSectionHeight();
+      },
+
+      updateSectionHeight: function() {
+        var sectionCount = this.$('section').length;
+        var height = window.innerHeight - (((sectionCount-1) * 50) + (2 * 49));
+        this.$('section.active').attr('style', 'height:' + height + 'px');
       }
 
     });

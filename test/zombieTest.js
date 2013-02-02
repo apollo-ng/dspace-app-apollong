c874b34bed04ac2c1ae9968e7019620015a3c166
elf Pavlik <perpetual-tripper@wwelves.org>
2013-02-02 15:11:42 +0100

    ender fails

--- test/zombieTest.js
+++ test/zombieTest.js
@@ -4,7 +4,11 @@ var assert = require('assert');
 describe('just checking', function(){
   before(function(done){
     this.browser = new zombie.Browser();
-    this.browser.visit('http://localhost:3000/index.dev.html');
+    this.browser.visit('http://localhost:3000/index.dev.html')
+      .then(done, done)
+      .fail(function(){
+        console.log('fail');
+      });
   });
 
   it('says true', function(){

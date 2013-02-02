a6c205ad545ab5757eac11ec4c3942d0cb463964
elf Pavlik <perpetual-tripper@wwelves.org>
2013-02-03 00:10:59 +0100

    random stuff...

--- test/runner.js
+++ test/runner.js
@@ -1,17 +1,31 @@
-mocha.setup('bdd');
+require(["ender"], function($){
+  mocha.setup('bdd');
 
-mocha.globals(['provide', '$', 'ender']);
+  mocha.globals(['provide', '$', 'ender', '_', 'Backbone']);
 
-function assert(expr, msg) {
-  if (!expr) throw new Error(msg || 'failed');
-}
+  var expect = chai.expect;
 
-describe('something...', function(){
+  describe('something...', function(){
 
-  it('always true', function(){
-    assert(true);
+    before(function(done){
+      navigator.geolocation = {};
+      navigator.geolocation.watchPosition = function(){
+        console.log('WATCH POSITION called');
+      };
+      setTimeout(function() {
+        done();
+      }, 8000);
+    });
+
+    it('always true', function(){
+      expect($('.featureTab').length).to.be.equal(2);
+    });
+
+    it('always false', function(){
+      expect(false).to.be.true;
+    });
   });
-});
 
-if (window.mochaPhantomJS) { mochaPhantomJS.run(); }
-else { mocha.run(); }
+  if (window.mochaPhantomJS) { mochaPhantomJS.run(); }
+  else { mocha.run(); }
+});

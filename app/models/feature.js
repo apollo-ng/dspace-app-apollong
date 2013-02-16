4a7deb63d92be53481f8da175aef23da6b2018bd
iggy <iggy@yggi.de>
2013-02-17 00:54:06 +0100

    calculate distance to user (#50)

--- app/models/feature.js
+++ app/models/feature.js
@@ -57,6 +57,46 @@ define([
       geometry.coordinates = [lon, lat];
       this.set('geometry', geometry);
       this.trigger('position-changed', this.getLatLon());
+    },
+    
+    /**
+     * Method: distanceTo
+     * 
+     * returns the distance in km to another <Feature>
+     */
+    distanceTo: function(feature) {
+      
+      //Calculation from #50
+      var distance = function (lat1, lon1, lat2, lon2, unit){
+        var radlat1 = Math.PI * lat1/180
+        var radlat2 = Math.PI * lat2/180
+        var radlon1 = Math.PI * lon1/180
+        var radlon2 = Math.PI * lon2/180
+        var theta = lon1-lon2
+        var radtheta = Math.PI * theta/180
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        dist = Math.acos(dist)
+        dist = dist * 180/Math.PI
+        dist = dist * 60 * 1.1515
+        if (unit=="K") { dist = dist * 1.609344 }
+        if (unit=="N") { dist = dist * 0.8684 }
+        var result = [];
+      
+        if (dist < 1 && unit == 'K') {
+          result[0]=Math.round(dist * 1000);
+          result[1]="m";
+        } else {
+          result[0]=Math.round(dist);
+          result[1]="km";
+        }
+        return result;
+      }
+      var d = distance(this.getLatLon().lat,this.getLatLon().lon, feature.getLatLon().lat, feature.getLatLon().lon, "K");
+      if (isNaN(d[0])) {
+        return "?"
+      } else {
+      return d[0]+d[1];
+      }
     }
   });
 
